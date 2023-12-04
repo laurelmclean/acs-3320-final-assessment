2590d4ed5e775e9ee60240f3da7144a46e3c8336
Laurel McLean <laurel.mclean@live.ca>
2023-12-04 12:46:36 -0700

    first commit

---
+++ ticker-tape.js
@@ -0,0 +1,30 @@
+class TickerTape extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._text = this.textContent;
+        this._duration = parseInt(this.getAttribute('duration')) || 1000;
+        this._tickerText = document.createElement('div');
+        this._shadowRoot.appendChild(this._tickerText);
+        this._tickerText.classList.add('ticker-text');
+        this._currentIndex = 0;
+        this._updateText();
+    }
+
+    connectedCallback() {
+        this._timer = setInterval(() => {
+            this._currentIndex = (this._currentIndex + 1) % this._text.length;
+            this._updateText();
+        }, this._duration);
+    }
+
+    disconnectedCallback() {
+        clearInterval(this._timer);
+    }
+
+    _updateText() {
+        this._tickerText.textContent = this._text.substring(this._currentIndex) + this._text.substring(0, this._currentIndex);
+    }
+}
+
+customElements.define('ticker-tape', TickerTape);
